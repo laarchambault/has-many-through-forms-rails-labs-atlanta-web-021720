2e8e698a979fd4ed3614e4f86eee901c3558c310
laarchambault <laarchambault@gmail.com>
2020-03-20 08:32:23 -0400

    need comments next

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,6 +5,8 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+Post.destroy_all
+Category.destroy_all
 
 5.times do
     Post.create(title: Faker::Book.title, content: Faker::Quote.famous_last_words)
@@ -12,4 +14,16 @@ end
 
 10.times do
     Category.create(name: Faker::Commerce.department)
+end
+
+10.times do
+    PostCategory.create(post_id: Post.all.sample.id, category_id: Category.all.sample.id)
+end
+
+10.times do
+    User.create(username: Faker::Internet.username, email: Faker::Internet.email)
+end
+
+20.times do
+    Comment.create(content: Faker::TvShows::NewGirl.quote, user_id: User.all.sample.id, post_id: Post.all.sample.id)
 end
